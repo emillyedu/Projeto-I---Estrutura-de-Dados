7924f7a30027519df9b5ee6b7234b8c17daff4dc
LucasBitu <lucasbitu55@gmail.com>
2023-05-23 15:01:54 -0300

    Filas e Pilhas interface

--- ProjetoED.py
+++ ProjetoED.py
@@ -458,17 +458,17 @@ def tela3():
 def telaFila():
     
     TAM_MAX = 21 # tamanho máximo da lista como definido em listaSeq
-    fila = FilaSeq()
+    lista = ListaSeq()
 
     ######################## FUNCOES ###################
 
-    def gerar_view():
+    def gerar_view(posicao_busca=None):
 
         listaview.delete("all")
-
+        
         square_width = 50
         square_height = 50  
-        x_gap = 0
+        x_gap = 50
         y_gap = 20
         max_x = 1000
         max_y = 420
@@ -476,72 +476,76 @@ def telaFila():
         y = y_gap
 
         # Loop para desenhar os quadrados e as setas
-        # Desenha o quadrado
+        for posicao in range(1, lista.tamanho() + 1):
+            # Desenha o quadrado
 
-        text_x = x + square_width / 2
-        text_y = y + square_height / 2
-        max_text_width = square_width - 10
+            text_x = x + square_width / 2
+            text_y = y + square_height / 2
+            max_text_width = square_width - 10
 
-        fill_color = "#4493C7"
-        # if posicao == posicao_busca  and posicao != None:
-        #     fill_color = "#44C764"
+            fill_color = "#FFD700"
+            if posicao == posicao_busca:
+                fill_color = "#FF8C00"
 
-        # valor = fila.elemento(posicao)
-        # if  valor == None:
-        #     fill_color = "#B4EDFC"
-        # elif valor == busca:  # se o elemento for o buscado alterar a cor da caixa
-        #     fill_color = "#44C764"
+            square = listaview.create_rectangle(x, y, x+square_width, y+square_height, fill=fill_color)
 
-        square = listaview.create_rectangle(x, y, x+square_width, y+square_height, fill=fill_color)
+            valor = lista.elemento(posicao)
+            listaview.create_text(text_x, text_y, text=str(valor), width=max_text_width)
 
-        valor = fila.primeiro()
-        listaview.create_text(text_x, text_y, text=str(valor), width=max_text_width)
+            arrow = listaview.create_line(x+square_width, y+square_height/2, x+square_width+x_gap, y+square_height/2, arrow=tk.LAST)
 
-        # Verifica se o quadrado chegou ao limite horizontal
-        if x + square_width + x_gap > max_x:
-            # Passa para a próxima linha
-            y = y + square_height + y_gap
-            if y > max_y:
-                return KeyError("Lista muito grande")
-            x = x_gap
-            arrow = listaview.create_line(0, y+square_height/2, x, y+square_height/2)
+            # Verifica se o quadrado chegou ao limite horizontal
+            if x + square_width + x_gap > max_x:
+                # Passa para a próxima linha
+                y = y + square_height + y_gap
+                if y > max_y:
+                    return KeyError("Lista muito grande")
+                x = x_gap
+                arrow = listaview.create_line(0, y+square_height/2, x, y+square_height/2, arrow=tk.LAST)
 
-        else:
-            # Move para a direita
-            x = x + square_width + x_gap
+            else:
+                # Move para a direita
+                x = x + square_width + x_gap
 
     def inserir():
         valor = int(caixa1.get())
-        if fila.insere(valor):
-            print("Inserido com sucesso")
+        if lista.vazia():
+            lista.insere(1, valor)
+            print("Inserido início")
             caixa1.delete(0, tk.END)
             gerar_view()
         else:
-            print("Erro ao inserir")
-            messagebox.showerror("Erro", "Erro ao inserir")
+            if lista.insere((lista.tamanho())+1, valor):
+                print("Inserido com sucesso")
+                caixa1.delete(0, tk.END)
+                gerar_view()
+            else:
+                print("Erro ao inserir")
+                messagebox.showerror("Erro", "Erro ao inserir")
 
     def remover():
-        if fila.remove() != None:
+        if lista.remove(1) != None:
             print("Removido com sucesso")
 
             gerar_view()
         else:
             print("Erro ao remover")
-            messagebox.showerror("Erro", "Erro ao remover")
+            messagebox.showerror("Erro", "Erro ao remover, posição inválida")
     
-    def primeiro_elemento():
+    def busca_posicao():
 
-        if fila.primeiro() is not None:
-            print("O primeiro elemento é: ", fila.primeiro())
-            gerar_view()
+        if lista.elemento(1) is not None:
+            messagebox.showinfo("Busca", "O primeiro elemento da Fila é: {}" .format(lista.elemento(1)))
+            print("Busca feita com sucesso, o valor é: ", lista.elemento(1))
+            gerar_view(1)
         else:
             print("Erro ao buscar")
             messagebox.showerror("Erro", "Erro ao buscar por posição")
 
     ######################## INTERFACE GRAFICA (GUI) ###################
     
-    label.config(text="Fila Sequencial", bg='#44C7C7')
-    root.config(bg='#44C7C7')
+    label.config(text="Fila", bg='#F0E68C')
+    root.config(bg='#F0E68C')
     for widget in root.winfo_children():
         if widget != label:
             widget.destroy()
@@ -553,13 +557,13 @@ def telaFila():
     linha = tk.Frame(root, width=1080, height=1, bg='black')
 
     caixa1 = tk.Entry(root)
-    label_caixa1 = tk.Label(root, text="Insira o valor:", bg='#44C7C7')
+    label_caixa1 = tk.Label(root, text="Insira o valor:", bg='#F0E68C')
 
-    botao1 = tk.Button(root, text="inserir (informe valor)", command=inserir, width= 24)
-    botao2 = tk.Button(root, text="Remover (informe)", command= remover, width = 24)
-    botao3 = tk.Button(root, text="Exibe primeiro elemento", command= primeiro_elemento, width = 24)
+    botao1 = tk.Button(root, text="inserir na fila", command=inserir, width= 24)
+    botao2 = tk.Button(root, text="Remover primeira posição", command= remover, width = 24)
+    botao4 = tk.Button(root, text="Busca primeira posição", command= busca_posicao, width = 24)
     
-    visualizacao = tk.Label(root, text= "Fila: ", font=("Arial", 12), bg='#44C7C7')
+    visualizacao = tk.Label(root, text= "Fila: ", font=("Arial", 12), bg='#F0E68C')
     listaview = tk.Canvas(root, width= 1080, height= 490, bg= "white")
 
     ######################## POSICAO DOS COMPONENTES (LAYOUT) ###################
@@ -571,12 +575,132 @@ def telaFila():
 
     botao1.place(x=550, y=43)
     botao2.place(x=850, y=43)
-    botao3.place(x=550, y=80)
+    botao4.place(x=850, y=80)
 
     visualizacao.place(x= 360, y=230)
     listaview.place(x= 0, y= 250)
 
-    gerar_view(None)
+    gerar_view()
+
+def telaPilha():
+    
+    TAM_MAX = 21 # tamanho máximo da lista como definido em listaSeq
+    lista = ListaSeq()
+
+    ######################## FUNCOES ###################
+
+    def gerar_view(posicao_busca=None):
+        listaview.delete("all")
+        
+        square_width = 50
+        square_height = 50  
+        x_gap = 50
+        y_gap = 20
+        max_x = 1000
+        max_y = 450
+        x = x_gap
+        y = max_y - square_height - y_gap  # Inicializa a posição y no limite máximo inferior
+
+        for posicao in range(1, lista.tamanho() + 1):
+            text_x = x + square_width / 2
+            text_y = y + square_height / 2
+            max_text_width = square_width - 10
+
+            fill_color = "#DEB887"
+            if posicao == posicao_busca:
+                fill_color = "#F5DEB3"
+
+            square = listaview.create_rectangle(x, y, x+square_width, y+square_height, fill=fill_color)
+
+            valor = lista.elemento(posicao)
+            listaview.create_text(text_x, text_y, text=str(valor), width=max_text_width)
+
+            arrow = listaview.create_line(x+square_width/2, y, x+square_width/2, y-y_gap, arrow=tk.LAST)
+
+            if y - square_height - y_gap < 0:  # Verifica se chegou ao limite superior
+                x = x + square_width + x_gap
+                if x > max_x:
+                    return KeyError("Lista muito grande")
+                y = max_y - square_height - y_gap
+                arrow = listaview.create_line(x+square_width/2, max_y, x+square_width/2, y+square_height, arrow=tk.LAST)
+            else:
+                y = y - square_height - y_gap
+
+
+    def inserir():
+        valor = int(caixa1.get())
+        if lista.vazia():
+            lista.insere(1, valor)
+            print("Inserido início")
+            caixa1.delete(0, tk.END)
+            gerar_view()
+        else:
+            if lista.insere((lista.tamanho())+1, valor):
+                print("Inserido com sucesso")
+                caixa1.delete(0, tk.END)
+                gerar_view()
+            else:
+                print("Erro ao inserir")
+                messagebox.showerror("Erro", "Erro ao inserir")
+
+    def remover():
+        if lista.remove(lista.tamanho()) != None:
+            print("Removido com sucesso")
+
+            gerar_view()
+        else:
+            print("Erro ao remover")
+            messagebox.showerror("Erro", "Erro ao remover, posição inválida")
+    
+    def busca_posicao():
+
+        if lista.elemento(lista.tamanho()) is not None:
+            messagebox.showinfo("Busca", "O topo da pilha é: {}" .format(lista.elemento(lista.tamanho())))
+            print("Busca feita com sucesso, o valor é: ", lista.elemento(lista.tamanho()))
+            gerar_view(lista.tamanho())
+        else:
+            print("Erro ao buscar")
+            messagebox.showerror("Erro", "Erro ao buscar por posição")
+
+    ######################## INTERFACE GRAFICA (GUI) ###################
+    
+    label.config(text="Pilha", bg='#DEB887')
+    root.config(bg='#DEB887')
+    for widget in root.winfo_children():
+        if widget != label:
+            widget.destroy()
+
+    botoes_iniciais()
+
+    ######################## COMPONENTES (WIDGETS) ###################
+    
+    linha = tk.Frame(root, width=1080, height=1, bg='black')
+
+    caixa1 = tk.Entry(root)
+    label_caixa1 = tk.Label(root, text="Insira o valor:", bg='#DEB887')
+
+    botao1 = tk.Button(root, text="inserir na pilha", command=inserir, width= 24)
+    botao2 = tk.Button(root, text="Remover da pilha", command= remover, width = 24)
+    botao4 = tk.Button(root, text="Busca topo pilha", command= busca_posicao, width = 24)
+    
+    visualizacao = tk.Label(root, text= "Pilha: ", font=("Arial", 12), bg='#DEB887')
+    listaview = tk.Canvas(root, width= 1080, height= 490, bg= "white")
+
+    ######################## POSICAO DOS COMPONENTES (LAYOUT) ###################
+
+    linha.place(x=0, y=240)
+
+    caixa1.place(x=300, y=43)
+    label_caixa1.place(x=300, y=20)
+
+    botao1.place(x=550, y=43)
+    botao2.place(x=850, y=43)
+    botao4.place(x=850, y=80)
+
+    visualizacao.place(x= 360, y=230)
+    listaview.place(x= 0, y= 250)
+
+    gerar_view()
 
 root = tk.Tk()
 root.resizable(False, False)
@@ -608,8 +732,12 @@ def botoes_iniciais():
     botao3.place(x=0, y=79)
     
     # Botão para trocar para tela Fila
-    botao3 = tk.Button(root, text="Fila Sequencial", command=telaFila, width = 12)
-    botao3.place(x=0, y=106)
+    botao3 = tk.Button(root, text="Fila", command=telaFila, width = 12)
+    botao3.place(x=0, y=105)
+
+    # Botão para trocar para tela Fila
+    botao4 = tk.Button(root, text="Pilha", command=telaPilha, width = 12)
+    botao4.place(x=0, y=131)
     
 texto_inicial()
 botoes_iniciais()
